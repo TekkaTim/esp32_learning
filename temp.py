2b80cce783fbfa08e2ecf6560dbfb804c4c49dcb
TekaTim <tekka@smartclan.com>
2018-02-15 12:47:00 +1100

    Started making changes

--- temp.py
+++ temp.py
@@ -1,1 +1,7 @@
-#This file gets the temperature from the pysense board
+#This file gets the temperature from the pysense board from the Si7006-A20 chip.
+
+from machine import I2C
+from machine import Pin
+import time
+
+i2c = I2C()
