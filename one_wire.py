a55c7f111d0e923e16a2e6fafd3a1ac2c9c999ac
Tim <tim@falchion.tekka.lan>
2018-03-05 23:04:05 +1100

    Started adding more sensors
    Tried to add 2 sensors. I can see them when I scan the bus, but can't address them individually yet...

--- one_wire.py
+++ one_wire.py
@@ -7,13 +7,40 @@ from onewire import OneWire
 ow = OneWire(Pin('P10'))
 temp = DS18X20(ow)
 
-result=OneWire.scan(ow)
-print(result)
-temp.start_convertion()
-time.sleep(1)
+#ds = DS18X20(OneWire(Pin('P10')))
+#print('devices:', ds.roms)
+##devices: [bytearray(b'(\xff\xae8\x90\x15\x03T')]
+#print('temperatures:', ds.read_temps())
+##temperatures: [2525]
 
+
+devices = ow.scan()
+#roms = ds.scan()
+print('found probes:', devices)
+#print('found probes:', roms)
+ 
 while True:
-    print(temp.read_temp_async())
-    time.sleep(1)
-    temp.start_convertion()
-    time.sleep(1)
+    print('temperatures:', end=' ')
+    #ow.convert_temp()  
+    for device in devices:
+        temp.start_convertion(device)
+        time.sleep(1)
+#        ow.select_rom(device)
+#Doesnt really work for selection the sesnor, gives "bound_method" response :-(
+        print(temp.read_temp_async, end=' ')
+    print()
+#    time.sleep_ms(1000)
+
+
+
+
+
+
+#temp.start_convertion()
+#time.sleep(1)
+
+#while True:
+#    print(temp.read_temp_async())
+#    time.sleep(1)
+#    temp.start_convertion()
+#    time.sleep(1)
