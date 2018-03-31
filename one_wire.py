dcc6fa83ed5afe1690fff26d9aebdfd89c3d825f
Tim <tim@falchion.tekka.lan>
2018-03-31 21:14:18 +1100

    Got device select working
    Got device select working for the temp sensors.
     select working
    
    Got device select working for the temp sensors. Currently polling one after the other.Currently polling one after the other.

--- one_wire.py
+++ one_wire.py
@@ -6,33 +6,23 @@ from onewire import OneWire
 #DS18B20 data line connected to pin P10
 ow = OneWire(Pin('P10'))
 temp = DS18X20(ow)
-
 print('Scanning the One Wire Bus....')
 devices = ow.scan()
+num_devices=len(devices)
 
 print('The following devices have been found....')
-for device_serial_array in devices:
-  #hex_string="".join("%02x" %b for b in device_serial_array)
-  #print(hex_string)
-  device_serial="".join("%02x" % device_serial_array[c-1] for c in range(len(device_serial_array), 0, -1))
+for device in devices:
+  device_serial="".join("%02x" % device[c-1] for c in range(len(device), 0, -1))
   print("  * Serial Number = ", device_serial)
 
 
+while True:
+  for device in devices:
+    temp.start_convertion(device)
+    time.sleep_ms(750)
+    print(temp.read_temp_async(device))
+    #time.sleep_ms(500)
 
-#for number in devices
-#    print(devices[number])
-
- 
-#while True:
-#    print('temperatures:', end=' ')
-#    #ow.convert_temp()
-#    for device in devices:
-#        temp.start_convertion(device)
-#        time.sleep(1)
-#        ow.select_rom(device)
-##Doesnt really work for selection the sesnor, gives "bound_method" response :-(
-#        print(temp.read_temp_async, end=' ')
-#    print()
 #    time.sleep_ms(1000)
 
 
