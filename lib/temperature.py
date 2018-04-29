b385e4edf642f93d8ae1e61c32c4d8c3af8bfecc
TekaTim <tekka@smartclan.com>
2018-04-29 21:22:12 +1000

    Fixed Temperature to 1 Decimal Point

--- lib/temperature.py
+++ lib/temperature.py
@@ -2,9 +2,11 @@
 # Subroutines
 ##########################
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 def Calc_Temp(centicelcius):
   Correct_Temp=centicelcius/100
+  #Correct_Temp=float("%1.f" % Correct_Temp)
+  Correct_Temp=float(round(Correct_Temp,1))
   return Correct_Temp
 
