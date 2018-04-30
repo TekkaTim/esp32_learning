64ae16792ab9afe78429ed8bc9138525e3ba8d28
Tim Smart <tekka@smartclan.com>
2018-04-30 22:09:42 +1000

    Added the changes from Falchion I forgot to commit
      * Made Temp Sensors one Dictionary
      * Added Fan Dictionary

--- one_wire.py
+++ one_wire.py
@@ -10,34 +10,59 @@ WLAN_Connect('gritz')
 #DS18B20 data line connected to pin P10
 ow = OneWire(Pin('P10'))
 temp = DS18X20(ow)
-temp_serials = {"Front" : bytearray(b'\x28\x89\x74\x29\x07\x00\x00\x89'),
-                "Back"  : bytearray(b'\x28\xb3\x31\x29\x07\x00\x00\x90')}
-num_devices=len(temp_serials)
-temperature = { "Front" : 0,
-                "Back"  : 0}
-device_read = { "Front" : 0,
-                "Back"  : 0}
-temp_sensors = { "Front" : { "serial" : bytearray(b'\x28\x89\x74\x29\x07\x00\x00\x89'),
+
+temp_sensors = { "Intake" : { "serial" : bytearray(b'\x28\x89\x74\x29\x07\x00\x00\x89'),
                              "available" : 0,
-                             "temperature" : 22,
-                             "read" : 33 },
-                 "Back"  : { "serial" : bytearray(b'\x28\xb3\x31\x29\x07\x00\x00\x90'),
+                             "celcius" : 0,
+                             "read" : 0 },
+#                 "HDD1"  : { "serial" : bytearray(b'\x28\xb3\x31\x29\x07\x00\x00\x90'),
+#                             "available" : 0,
+#                             "celcius" : 0,
+#                             "read" : 0 },
+#                 "HDD2"  : { "serial" : bytearray(b'\x28\xb3\x31\x29\x07\x00\x00\x90'),
+#                             "available" : 0,
+#                             "celcius" : 0,
+#                             "read" : 0 },
+#                 "Hot_Isle" : { "serial" : bytearray(b'\x28\xb3\x31\x29\x07\x00\x00\x90'),
+#                             "available" : 0,
+#                             "celcius" : 0,
+#                             "read" : 0 },
+                 "Exhaust"  : { "serial" : bytearray(b'\x28\xb3\x31\x29\x07\x00\x00\x90'),
                              "available" : 0,
-                             "temperature" : 44,
-                             "read" : 55 } }
+                             "celcius" : 0,
+                             "read" : 0 }
+               }
+
+num_devices=len(temp_sensors)
+
+#Fan RPM rate at certain temperatures.
+fan_speed_vs_temp = { "Intake" : {
+                        "very_low":15,"low":18,"medium":20,"high":23,"very_high":25,"extreme":28 },
+#                      "HDD1" : {
+#                        "very_low":20,"low":25,"medium":30,"high":35,"very_high":40,"extreme":45 },
+#                      "HDD2" : {
+#                        "very_low":20,"low":25,"medium":30,"high":35,"very_high":40,"extreme":45 },
+#                      "Hot_Isle" : {
+#                        "very_low":20,"low":25,"medium":30,"high":35,"very_high":40,"extreme":45 },
+                      "Exhaust" : {
+                        "very_low":20,"low":25,"medium":30,"high":35,"very_high":40,"extreme":45 }
+                    }
+#Fan RPM Speeds.
+fan_rpm_speeds = { "very_low":30, "low":40, "medium":50, "high":60, "very_high":80, "extreme":100 }
+
 
 print('Scanning the One Wire Bus....')
 devices = ow.scan()
 num_devices_found=len(devices)
 
-
 print('Looking for the following devices....')
-for key in temp_serials:
+for key in temp_sensors:
   sensor_found="Missing"
-  if temp_serials[key] in devices: sensor_found="Available"
-  if temp_serials[key] in devices: temp_sensors[key]["available"]=1
-  device_serial="".join("%02x" % temp_serials[key][c-1] for c in range(len(temp_serials[key]), 0, -1))
+  if temp_sensors[key]["serial"] in devices: sensor_found="Available"
+  if temp_sensors[key] in devices: temp_sensors[key]["available"]=1
+  device_serial="".join("%02x" % temp_sensors[key]["serial"][c-1] for c in range(len(temp_sensors[key]["serial"]), 0, -1))
   print("  * ", key, " Serial Number = ", device_serial, " (",sensor_found,")")
+
 
 # PUT IN A CHECK FOR NEW DEVICES
 
@@ -46,30 +71,47 @@ for key in temp_serials:
 
 while True:
 #for x in range(1):
+  # CHECK TEMPERATURE SENSORS
   temp_not_done=1
   temp_retry=0
-  for key in temp_serials:
+  for key in temp_sensors:
     if (temp_sensors[key]["available"] == 1):
-      temp.start_convertion(temp_serials[key])
+      temp.start_convertion(temp_sensors[key]["serial"])
       time.sleep_ms(10)
 
   while True:
     temp_retry+=1
     if ( temp_retry > 20 ):
+      print("Retries for sensors exceeded")
       break
-    for key in temp_serials:
+    for key in temp_sensors:
       if (temp_sensors[key]["available"] == 1):
-        if (device_read[key] != 1):
-          temp_data = (temp.read_temp_async(temp_serials[key]))
+        if (temp_sensors[key]["read"] != 1):
+          temp_data = (temp.read_temp_async(temp_sensors[key]["serial"]))
           if (temp_data != None):
             temp_data = Calc_Temp(temp_data)
-            temperature[key] = temp_data
-            device_read[key] = 1
+            temp_sensors[key]["celcius"] = temp_data
+            temp_sensors[key]["read"] = 1
         time.sleep_ms(100)
-    if ( sum(device_read.values()) == num_devices ):
-      break
-  for key in temperature:
+      read_count=0
+      for key in temp_sensors:
+        read_count += temp_sensors[key]["read"]
+      if ( read_count >= num_devices ):
+        break
+
+
+  for key in temp_sensors:
     if (temp_sensors[key]["available"] == 1):
-      print("Temp", key, " = ", temperature[key])
-      device_read[key]=0
+      print("Temp", key, " = ",temp_sensors[key]["celcius"])
+      temp_sensors[key]["read"]=0
+
+
+
+  # CHECK FAN DATA
+
+
+  # ASSESS TEMPERATURE VS FAN DATA
+#  rpm_rate_required=0
+#  for key in temp_sensors:
+#    for rate in fan_rpm_speeds
 
